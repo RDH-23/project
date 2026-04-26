d5ce2c92c3884e48bd447fc23b1107ec121f604b
void-coder <restu.haqiqi23@gmail.com>
2026-04-26 11:24:47 +0700

    commit pertama

--- assets/js/daftar-film.js
+++ assets/js/daftar-film.js
@@ -1,36 +1,6 @@
 // Inisialisasi ikon Feather dari Wisnu
 feather.replace();
 
-// 1. Logika Dropdown Navbar (Dari Wisnu)
-document.addEventListener("DOMContentLoaded", () => {
-  const dropdownTrigger = document.getElementById("about-us-trigger");
-  const dropdownMenu = document.getElementById("about-us-menu");
-  const dropdownLinks = dropdownMenu.querySelectorAll("a");
-
-  dropdownTrigger.addEventListener("click", (event) => {
-    event.stopPropagation();
-    dropdownMenu.classList.toggle("show");
-    dropdownTrigger.classList.toggle("active");
-  });
-
-  dropdownLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      dropdownMenu.classList.remove("show");
-      dropdownTrigger.classList.remove("active");
-    });
-  });
-
-  document.addEventListener("click", (event) => {
-    if (
-      !dropdownMenu.contains(event.target) &&
-      !dropdownTrigger.contains(event.target)
-    ) {
-      dropdownMenu.classList.remove("show");
-      dropdownTrigger.classList.remove("active");
-    }
-  });
-});
-
 // 2. Logika Interaksi Tambahan (Klik Poster Film)
 const movieCards = document.querySelectorAll(".movie-card");
 movieCards.forEach((card) => {
